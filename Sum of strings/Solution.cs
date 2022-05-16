5ea437765ccb80d1c7e20efa54469cf49ca6ff01
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2022-05-16 11:23:54 +0300

    Add project files.

---
+++ Sum of strings/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+
+public static class Kata
+{
+    public static string sumStrings(string a, string b)
+    {
+        if (string.IsNullOrWhiteSpace(a))
+            a = "0";
+        if (string.IsNullOrWhiteSpace(b))
+            b = "0";
+
+        return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+    }
+}
